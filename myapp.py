91b04a89274e80d9467482fc45fcfaba56d63c4f
Novak Sepelj <novaksepelj11@gmail.com>
2024-04-01 23:01:01 +0200

    v.12

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Docker v.11!'
+    return 'Hello, Docker v.12!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
