fed23ebe8ca0d9edabeafbc4c7b13c21ada615c1
Novak Sepelj <novaksepelj11@gmail.com>
2024-04-04 13:46:54 +0200

    v.12

--- myapp.py
+++ myapp.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('ime.html')
+    return 'Hello, Docker v.12!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
